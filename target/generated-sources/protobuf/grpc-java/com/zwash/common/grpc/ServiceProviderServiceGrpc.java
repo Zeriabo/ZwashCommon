731adeb4c00ab9af43912ffa5c87421234d4c407
Zeriab Chiah <zeriab@Zeriabs-MBP.lan>
2025-08-08 11:05:16 +0300

    grpc

--- target/generated-sources/protobuf/grpc-java/com/zwash/common/grpc/ServiceProviderServiceGrpc.java
+++ target/generated-sources/protobuf/grpc-java/com/zwash/common/grpc/ServiceProviderServiceGrpc.java
@@ -4,7 +4,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 
 /**
  */
-@jakarta.annotation.Generated(
+@javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.63.0)",
     comments = "Source: service_provider.proto")
 @io.grpc.stub.annotations.GrpcGenerated
